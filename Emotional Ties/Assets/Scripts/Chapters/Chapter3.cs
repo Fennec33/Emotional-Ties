f6adae3dcf27480538565cb460567858ae0eeb28
Fennec33 <66853066+Fennec33@users.noreply.github.com>
2022-06-25 17:52:13 -0700

    Wired up logic for chapter 3

--- Emotional Ties/Assets/Scripts/Chapters/Chapter3.cs
+++ Emotional Ties/Assets/Scripts/Chapters/Chapter3.cs
@@ -29,11 +29,122 @@ namespace Chapters
         [SerializeField] private Conversation afraidOfAffair;
         [SerializeField] private Conversation mysteriousWoman;
         [SerializeField] private Conversation questioningEnd;
+
+        private bool _paniced = false;
+        private bool _overwhelmed = false;
+        private bool _alreadyAskedSawMichael = false;
+        private int _allQuestionsAskedMax = 4;
+        private int _allQuestionsAsked = 0;
         
         private void AddStartingActionsAndAspects()
         {
             EncounterBoardManager.AddAspectToBoard(focus);
             EncounterBoardManager.AddAspectToBoard(traumaticMemories);
+            EncounterBoardManager.AddAspectToBoard(emotionallyDrained);
+            
+            ActionMenuManager.AddActionToMenu(BigFightLastWeek, "I heard you and Mr. Baker had a fight");
+            ActionMenuManager.AddActionToMenu(RememberAnythingElse, "Remember any information");
+            ActionMenuManager.AddActionToMenu(WitnessSawYou, "I know you saw Mr. Baker today");
+        }
+
+        public void BigFightLastWeek()
+        {
+            if (_overwhelmed)
+            {
+                DialogueManager.StartDialogue(this, overwhelmedIDidNotDoIt);
+            }
+            else if (_paniced)
+            {
+                DialogueManager.StartDialogue(this, iDidNotDoIt);
+            }
+            else
+            {
+                DialogueManager.StartDialogue(this, nothingToWorryAbout);
+            }
+        }
+
+        public void RememberAnythingElse()
+        {
+            if (_overwhelmed)
+            {
+                DialogueManager.StartDialogue(this, overwhelmedIDidNotDoIt);
+            }
+            else if (_paniced)
+            {
+                DialogueManager.StartDialogue(this, iDidNotDoIt);
+            }
+            else
+            {
+                DialogueManager.StartDialogue(this, nothingNew);
+            }
+        }
+
+        public void WitnessSawYou()
+        {
+            if (_overwhelmed)
+            {
+                DialogueManager.StartDialogue(this, pushingOnWitness);
+            }
+            else
+            {
+                if (_alreadyAskedSawMichael)
+                {
+                    DialogueManager.StartDialogue(this, theyAreLyingRepeat);
+                }
+                else
+                {
+                    DialogueManager.StartDialogue(this, theyAreLying);
+                }
+            }
+        }
+
+        private void PleaseTalkWithUs()
+        {
+            DialogueManager.StartDialogue(this, somethingIDidNotTellYou);
+        }
+
+        private void OverwhelmHer()
+        {
+            DialogueManager.StartDialogue(this, breakingDown);
+        }
+
+        private void CleanUpForSecondHalfOfChapter()
+        {
+            ActionMenuManager.RemoveAllActionsFromMenu();
+            ActionMenuManager.AddActionToMenu(WhenYouReallySawMichael, "When was the last time you saw Mr. Baker");
+        }
+
+        private void WhenYouReallySawMichael()
+        {
+            DialogueManager.StartDialogue(this, theRealLastConversation);
+        }
+
+        public void UpsetWithWork()
+        {
+            DialogueManager.StartDialogue(this, upsetWithWork);
+        }
+
+        public void GrowingDistant()
+        {
+            DialogueManager.StartDialogue(this, growingDistant);
+        }
+
+        public void AfraidOfAffair()
+        {
+            DialogueManager.StartDialogue(this, afraidOfAffair);
+        }
+
+        public void MysteriousWoman()
+        {
+            DialogueManager.StartDialogue(this, mysteriousWoman);
+        }
+
+        private void CheckIfAllQuestionsAsked()
+        {
+            if (_allQuestionsAsked >= _allQuestionsAskedMax)
+            {
+                DialogueManager.StartDialogue(this, questioningEnd);
+            }
         }
         
         public override void ConversationFinished(Conversation finishedConversation)
@@ -45,59 +156,80 @@ namespace Chapters
             }
             else if (finishedConversation == theyAreLying)
             {
-                //TODO
+                EncounterBoardManager.AddAspectToBoard(panic);
+                _alreadyAskedSawMichael = true;
             }
             else if (finishedConversation == theyAreLyingRepeat)
             {
-                //TODO
+                //nothing
             }
             else if (finishedConversation == breakingDown)
             {
-                //TODO
+                EncounterBoardManager.RemoveAspectFromBoard(emotionallyDrained);
+                EncounterBoardManager.RemoveAspectFromBoard(panic);
+                EncounterBoardManager.AddAspectToBoard(overwhelmed);
+                _overwhelmed = true;
+                _paniced = false;
+                ActionMenuManager.RemoveActionFromMenu("Please talk with us");
             }
             else if (finishedConversation == pushingOnWitness)
             {
-                //TODO
+                EncounterBoardManager.RemoveAspectFromBoard(overwhelmed);
+                _overwhelmed = false;
+                EncounterBoardManager.AddAspectToBoard(emotionallyDrained);
+                CleanUpForSecondHalfOfChapter();
             }
             else if (finishedConversation == nothingNew)
             {
-                //TODO
+                //nothing
             }
             else if (finishedConversation == nothingToWorryAbout)
             {
-                //TODO
+                ActionMenuManager.RemoveActionFromMenu("I heard you and Mr. Baker had a fight");
+                ActionMenuManager.AddActionToMenu(PleaseTalkWithUs, "Please talk with us");
             }
             else if (finishedConversation == somethingIDidNotTellYou)
             {
-                //TODO
+                CleanUpForSecondHalfOfChapter();
             }
             else if (finishedConversation == iDidNotDoIt)
             {
-                //TODO
+                //nothing
             }
             else if (finishedConversation == overwhelmedIDidNotDoIt)
             {
-                //TODO
+                //nothing
             }
             else if (finishedConversation == theRealLastConversation)
             {
-                //TODO
+                ActionMenuManager.AddActionToMenu(UpsetWithWork, "Why was he upset");
+                ActionMenuManager.AddActionToMenu(GrowingDistant, "Why where you growing distant");
+                ActionMenuManager.AddActionToMenu(AfraidOfAffair, "Was there an affair");
+                ActionMenuManager.AddActionToMenu(MysteriousWoman, "Who was the woman");
             }
             else if (finishedConversation == upsetWithWork)
             {
-                //TODO
+                ActionMenuManager.RemoveActionFromMenu("Why was he upset");
+                _allQuestionsAsked++;
+                CheckIfAllQuestionsAsked();
             }
             else if (finishedConversation == growingDistant)
             {
-                //TODO
+                ActionMenuManager.RemoveActionFromMenu("Why where you growing distant");
+                _allQuestionsAsked++;
+                CheckIfAllQuestionsAsked();
             }
             else if (finishedConversation == afraidOfAffair)
             {
-                //TODO
+                ActionMenuManager.RemoveActionFromMenu("Was there an affair");
+                _allQuestionsAsked++;
+                CheckIfAllQuestionsAsked();
             }
             else if (finishedConversation == mysteriousWoman)
             {
-                //TODO
+                ActionMenuManager.RemoveActionFromMenu("Who was the woman");
+                _allQuestionsAsked++;
+                CheckIfAllQuestionsAsked();
             }
             else if (finishedConversation == questioningEnd)
             {
